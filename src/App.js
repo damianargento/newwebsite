c24470f2875a5f8a13650ed07a26620bd44753e9
Argento <damian.argento@yrbrands.com>
2019-10-02 16:06:09 -0300

    Added all files of create-react-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const alturasArray = [1,100,200,300,400];
 
+const liStyle = { fontSize: '10px',
+				listStyleType:'none',
+				color:'#bebebe',
+				fontFamily:'Arial, sans-serif',}
+
+const divStyle= { display:'inline-flex', justifyContent:'space-around', alignSelf:'center', flexWrap:'wrap'}
+
+const alturas = alturasArray.map((altura,i) => <p key={i}>{altura}</p>);
+
+const Street = ({name,alturas}) => {return (<div><h1>{name}</h1><div style={liStyle}>{alturas}</div></div>)};
+
+const App = () => (
+	<div style={divStyle}>
+	<Street name="Congreso" alturas={alturas} />
+	<Street name="Arribe&ntilde;os" alturas={alturas} />
+	<Street name="Libertador"  alturas={alturas} />
+	<Street name="Cabildo" alturas={alturas} />
+	</div>
+);
 export default App;
