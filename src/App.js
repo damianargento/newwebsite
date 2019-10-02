700e495c4793ff8138c0d12b2c1d3345f1630734
Argento <damian.argento@yrbrands.com>
2019-10-02 18:54:43 -0300

    Adds more style to the menu and adds a mobile menu in the bottom. React icons imported

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
 import React from 'react';
 import './App.css';
+import {FaHome} from "react-icons/fa";
+import {FaPencilAlt} from "react-icons/fa";
+import {FaFile} from "react-icons/fa";
 
-let menuItems = [{title:"Home", url:"#home"}, {title:"Work", url:"#work"}, {title:"Articles", url:"#articles"}];
+
+let menuItems = [
+			{title:"Home", 
+			url:"#home", 
+			icon:<FaHome/> },
+
+			{title:"Work", 
+			url:"#work",
+			icon:<FaPencilAlt/>}, 
+
+			{title:"Articles",
+			url:"#articles",
+			icon:<FaFile />
+			}
+		];
+
 const headerStyle = { };
+
 const navStyle = { };
 
 const Menu = () => ( 
-			menuItems.map(
-				(item,i) => {return (<a href={item.url} key={i}>{item.title}</a>)}
-						)
+		menuItems.map(
+				(item,i) => {return (
+				<a href={item.url} key={i} align="center">
+						<span className="hide-desk">
+							{item.icon}
+							</span>
+							<span className="hide-mob">
+							{item.title}
+						</span> 
+				</a>)}
+						) 
 					)
-const App = () => ( 
+const MobileMenu = () => ( 	
+						<div className="menuStyle menuFooter hide-desk">
+						<Menu />
+						</div>
+						)
+					
+const App = () => (
+<div className="generalDiv">
 	<header style={headerStyle}>
-		<nav style={navStyle}>
-		    <div className="menuStyle">
+		<nav className="navStyle">
+		    <div className="menuStyle hide-mob">
 			   <Menu />
 			</div>
 		</nav>
 	</header>
+	<MobileMenu />
+</div>
 );
 export default App;
