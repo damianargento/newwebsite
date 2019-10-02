bab5aeb48d76a8a7cff77346cedce6e1b1d37162
Argento <damian.argento@yrbrands.com>
2019-10-02 17:57:57 -0300

    Styles the menu

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import './App.css';
 let menuItems = [{title:"Home", url:"#home"}, {title:"Work", url:"#work"}, {title:"Articles", url:"#articles"}];
 const headerStyle = { };
 const navStyle = { };
-const menuStyle = { width:'100%',display:'flex',flexWrap:'wrap', float:'left'};
 
 const Menu = () => ( 
 			menuItems.map(
-				(item) => {return (<a href={item.url}>{item.title}</a>)}
+				(item,i) => {return (<a href={item.url} key={i}>{item.title}</a>)}
 						)
 					)
 const App = () => ( 
 	<header style={headerStyle}>
 		<nav style={navStyle}>
-		    <div style={menuStyle}>
+		    <div className="menuStyle">
 			   <Menu />
 			</div>
 		</nav>
