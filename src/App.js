2e3ae5f3e915e681c04bac7fcfaf02a8a1c075ae
Argento <damian.argento@yrbrands.com>
2019-10-02 16:54:00 -0300

    started the website, just added the menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
 import './App.css';
 
-const alturasArray = [1,100,200,300,400];
+let menuItems = [{title:"Home", url:"#home"}, {title:"Work", url:"#work"}, {title:"Articles", url:"#articles"}];
+const headerStyle = { };
+const navStyle = { };
+const menuStyle = { width:'100%',display:'flex',flexWrap:'wrap', float:'left'};
 
-const liStyle = { fontSize: '10px',
-				listStyleType:'none',
-				color:'#bebebe',
-				fontFamily:'Arial, sans-serif',}
-
-const divStyle= { display:'inline-flex', justifyContent:'space-around', alignSelf:'center', flexWrap:'wrap'}
-
-const alturas = alturasArray.map((altura,i) => <p key={i}>{altura}</p>);
-
-const Street = ({name,alturas}) => {return (<div><h1>{name}</h1><div style={liStyle}>{alturas}</div></div>)};
-
+const Menu = () => ( 
+			menuItems.map(
+				(item) => {return (<a href={item.url}>{item.title}</a>)}
+						)
+					)
 const App = () => ( 
-	<div style={divStyle}>
-	<Street name="Congreso" alturas={alturas} />
-	<Street name="Arribe&ntilde;os" alturas={alturas} />
-	<Street name="Libertador"  alturas={alturas} />
-	<Street name="Cabildo" alturas={alturas} />
-	</div>
+	<header style={headerStyle}>
+		<nav style={navStyle}>
+		    <div style={menuStyle}>
+			   <Menu />
+			</div>
+		</nav>
+	</header>
 );
 export default App;
