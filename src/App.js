850583ea59ea9ff86c07c285ae1f48e3df599335
Argento <damian.argento@yrbrands.com>
2019-10-21 15:51:47 -0300

    This commit starts a weather app

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import {FaHome} from "react-icons/fa";
 import {FaPencilAlt} from "react-icons/fa";
 import {FaFile} from "react-icons/fa";
 
-
-let menuItems = [
+class Menu extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {menuItems: [
 			{title:"Home", 
 			url:"#home", 
 			icon:<FaHome/> },
@@ -18,12 +20,11 @@ let menuItems = [
 			url:"#articles",
 			icon:<FaFile />
 			}
-		];
-
-const headerStyle = { };
-
-const Menu = () => ( 
-		menuItems.map(
+		] }
+	}
+	render() {
+		return(
+		this.state.menuItems.map(
 				(item,i) => {return (
 				<a href={item.url} key={i} align="center">
 						<span className="hide-desk">
@@ -33,23 +34,74 @@ const Menu = () => (
 							{item.title}
 						</span> 
 				</a>)}
-						) 
-					)
+			)
+			) 
+			}
+		}
 const MobileMenu = () => ( 	
 						<div className="menuStyle menuFooter hide-desk">
 						<Menu />
 						</div>
 						)
-					
+
+	const API = "http://api.weatherstack.com/current?access_key=e85eaaed9d20b339b2db9017665a8ff9&query="
+
+class TimeApp extends React.Component {
+
+	
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			current: {},
+			ubicacion: {},
+			isLoading:false,
+			userQuery: "Buenos Aires"
+			}
+		}
+
+    componentDidMount() {
+		this.setState({isLoading:true})
+		fetch(API + this.state.userQuery)
+		.then(response => response.json())
+		.then(data => this.setState({ current: data.current, ubicacion: data.location, isLoading: false, }))
+}
+search = event => {
+	this.componentDidMount();
+}
+handleChange = event => {
+	this.setState({ userQuery: event.target.value });
+  };
+
+	render() {
+		const { current } = this.state;
+		const { ubicacion } = this.state;
+		const { isLoading } = this.state;
+		return (
+				isLoading ? <div>Cargando...</div> : 
+				<div>
+					<h1>{this.state.userQuery}</h1>
+				<div><img src={current.weather_icons} alt=""/></div>
+				<div>{ubicacion.name}</div>
+				<div>{current.temperature} °C</div>
+				<input value={this.state.userQuery} onChange={this.handleChange}></input>
+				<button onClick={this.search}>Search</button>
+				</div>
+			)
+		}
+	}
 const App = () => (
 <div className="generalDiv">
-	<header style={headerStyle}>
+	<header className="headerStyle">
 		<nav className="navStyle">
 		    <div className="menuStyle hide-mob">
 			   <Menu />
 			</div>
 		</nav>
 	</header>
+	<section className="Hero">
+		<TimeApp/>
+	</section>
 	<MobileMenu />
 </div>
 );
